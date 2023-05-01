36bf8b86ac6d59b0ad2a437078ae67b028f209e1
Amar Jyoti Patra <amarjyotipatra511@gmail.com>
2023-05-01 19:47:08 +0530

    logger-config added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { ServerConfig } = require('./config');
+const { ServerConfig,Logger } = require('./config');
 // console.log("Port",ServerConfig.PORT);
 const apiRoutes = require('./routes');
 
@@ -10,4 +10,5 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    Logger.info("Server successfully started")
 });
